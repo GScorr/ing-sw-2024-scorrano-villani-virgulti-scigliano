ceae70e3d7afd975f4ad1a34211348545a210c9f
francescoVirgulti <franci.virgulti@gmail.com>
2024-03-16 23:23:44 +0100

    aggiornamento Deck e CardClasses

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Deck/Deck.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Deck/Deck.java
@@ -2,11 +2,21 @@ package it.polimi.ingsw.model.Deck;
 import it.polimi.ingsw.model.Card.PlayCard;
 import java.util.ArrayDeque;
 import java.util.Deque;
+
+/*
+* @Francesco Virgulti
+* TODO:
+*  -getNumber()
+*  -drawFromDeck()
+*  - Implementa anche le sottoclassi
+* */
 abstract class Deck {
     Deque<PlayCard> cards = new ArrayDeque<>();
 
     public Deck(Deque<PlayCard> cards){
         this.cards = cards;
     }
+
+    public abstract PlayCard getStack();
 
 }
