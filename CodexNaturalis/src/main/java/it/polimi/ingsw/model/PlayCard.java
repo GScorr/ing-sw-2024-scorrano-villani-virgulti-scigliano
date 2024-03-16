c8f717c7e24dbe9ee6c0a71c870f0e2735c07166
francescoVirgulti <franci.virgulti@gmail.com>
2024-03-16 19:47:16 +0100

    aggiunta commento classe PlayCard

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayCard.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/PlayCard.java
@@ -8,24 +8,33 @@ import it.polimi.ingsw.model.ENUM.CentralEnum;
 *       - implementare metodo place()
 */
 public class PlayCard {
-    private final Side frontal_side ;
+    private final Side front_side ;
     private final  Side back_side;
     private boolean flipped;
     private final CentralEnum central_resources;
 
-    public PlayCard(Side frontal_side, Side back_side, boolean flipped, CentralEnum central_resources) {
-        this.frontal_side = frontal_side;
+    public PlayCard(Side front_side, Side back_side, boolean flipped, CentralEnum central_resources) {
+        this.front_side = front_side;
         this.back_side = back_side;
-        flippCard(flipped);
+        flipCard(flipped);
         this.central_resources = central_resources;
     }
 
-    public void flippCard(boolean flipped){
+    /*
+    * flipped = true => abilito getFrontSide
+    * flipped = false => abilito getBackSide*/
+    public void flipCard(boolean flipped){
         this.flipped = flipped;
     }
 
-    public Side getFrontalSide() {
-        return frontal_side;
+    /*
+    * flipped = true => result.contains(front_side)
+    * flipped = false => result=null
+    * throw new Error
+    * */
+
+    public Side getFrontSide() {
+        return front_side;
     }
 
     public Side getBackSide() {
