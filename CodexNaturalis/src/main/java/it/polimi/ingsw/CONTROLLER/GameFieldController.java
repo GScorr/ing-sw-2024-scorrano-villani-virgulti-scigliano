f7a6245b5bc2a95e95746b63514b1796eb78b2ef
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-03-19 17:36:57 +0100

    prova

--- CodexNaturalis/src/main/java/it/polimi/ingsw/CONTROLLER/GameFieldController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/CONTROLLER/GameFieldController.java
@@ -1,6 +1,6 @@
 package it.polimi.ingsw.CONTROLLER;
 
-import it.polimi.ingsw.MODEL.GameField;
+import it.polimi.ingsw.model.GameField;
 
 public class GameFieldController {
     private GameField player_field;
