e7a4412b82b3263c28abfcbaec3ccd92eaf2a67e
GScorr <gabriele.scorrano@mail.polimi.it>
2024-03-19 16:30:43 +0100

    aggiornamento Deck e CardClasses

--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Goal/GoalFeatherPenPaper.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Goal/GoalFeatherPenPaper.java
@@ -1,9 +1,10 @@
 package it.polimi.ingsw.model.Goal;
 
+import it.polimi.ingsw.model.ENUM.AnglesEnum;
 import it.polimi.ingsw.model.GameField;
 
 public class GoalFeatherPenPaper implements GoalStrategy{
-    public int totalPoints(GameField field, int points) {
+    public int totalPoints(GameField field, int points, AnglesEnum resource) {
         return 0;
     }
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/model/Goal/GoalLFigure.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/model/Goal/GoalLFigure.java
@@ -3,8 +3,8 @@ package it.polimi.ingsw.model.Goal;
 import it.polimi.ingsw.model.ENUM.AnglesEnum;
 import it.polimi.ingsw.model.GameField;
 
-public class GoalDoubleGold implements GoalStrategy{
+public class GoalLFigure implements GoalStrategy{
     public int totalPoints(GameField field, int points, AnglesEnum resource) {
-        return (int) (points*Math.floor(field.get()/2));
+        return 0;
     }
 }
