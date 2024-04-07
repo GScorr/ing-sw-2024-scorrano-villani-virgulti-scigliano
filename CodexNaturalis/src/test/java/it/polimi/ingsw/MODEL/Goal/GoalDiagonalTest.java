b6b9a694a1dec8a78755a25f07f5889ead6cb220
Mirko <mirko.scigliano9@gmail.com>
2024-04-07 10:36:36 +0200

    aggiornamento

--- CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/Goal/GoalDiagonalTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/Goal/GoalDiagonalTest.java
@@ -9,27 +9,47 @@ import it.polimi.ingsw.MODEL.GameField;
 import it.polimi.ingsw.MODEL.GameFieldSingleCell;
 import org.junit.jupiter.api.Test;
 
+//import org.json
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class GoalDiagonalTest {
     Side front = new Side(AnglesEnum.MUSHROOMS, AnglesEnum.MUSHROOMS, AnglesEnum.MUSHROOMS, AnglesEnum.MUSHROOMS, CentralEnum.ANIMAL, CentralEnum.ANIMAL, CentralEnum.ANIMAL);
     Side back = new Side(AnglesEnum.MUSHROOMS, AnglesEnum.MUSHROOMS, AnglesEnum.MUSHROOMS, AnglesEnum.MUSHROOMS, CentralEnum.ANIMAL, CentralEnum.ANIMAL, CentralEnum.ANIMAL);
-    PlayCard card1 = new ResourceCard(front, back, false, 1);
+    PlayCard card1 = new ResourceCard(front, back, false, 1); //usando le carte direttamente evito
     PlayCard card2 = new ResourceCard(front, back, false, 1);
-    GameFieldSingleCell gameFieldSingleCell1 = new GameFieldSingleCell(true, card1, AnglesEnum.MUSHROOMS, card2);
+    //prova[0] -> indica tutta carta devo comunque istanziarla manualmente tutta
+
+    //provare ad istanziare tutto il deck
+
+    PlayCard[] cards = new PlayCard[4];
+
+    public void istanziaCarte(){
+        for(int i=0; i<4; i++){
+            cards[i] = new ResourceCard();
+        }
+    }
+
+
+
+
+/*    GameFieldSingleCell gameFieldSingleCell1 = new GameFieldSingleCell(true, card1, AnglesEnum.MUSHROOMS, card2);
     GameFieldSingleCell gameFieldSingleCell2 = new GameFieldSingleCell(true, card1, AnglesEnum.MUSHROOMS, card2);
     GameFieldSingleCell gameFieldSingleCell3 = new GameFieldSingleCell(false, card1, AnglesEnum.MUSHROOMS, card2);
     GameFieldSingleCell gameFieldSingleCell4 = new GameFieldSingleCell(false, card1, AnglesEnum.MUSHROOMS, card2);
     GameFieldSingleCell gameFieldSingleCell5 = new GameFieldSingleCell(true, card1, AnglesEnum.MUSHROOMS, card2);
     GameFieldSingleCell gameFieldSingleCell6 = new GameFieldSingleCell(true, card1, AnglesEnum.MUSHROOMS, card2);
+*/
     GameFieldSingleCell[][] campo = new GameFieldSingleCell[4][4];
+
+    GameField gameField = new GameField(campo);
 
     @Test
     void totalPoints() {
 
+        gameField.insertCard(card1, 0, 0); //inserisco la carta nel gamefield
 
 
-        GameField gameField = new GameField(gameFieldSingleCell);
 
         GoalDiagonal goalDiagonal = new GoalDiagonal();
 
