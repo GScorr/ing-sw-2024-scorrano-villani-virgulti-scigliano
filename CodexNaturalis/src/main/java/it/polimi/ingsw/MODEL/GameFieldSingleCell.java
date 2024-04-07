9497d4e4d19489cb246cea14da3a6d9fef4398ec
Mirko <mirko.scigliano9@gmail.com>
2024-04-07 21:01:14 +0200

    aggiornamento

--- CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/GameFieldSingleCell.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/GameFieldSingleCell.java
@@ -1,11 +1,23 @@
 package it.polimi.ingsw.MODEL;
 
 import it.polimi.ingsw.MODEL.Card.PlayCard;
+import it.polimi.ingsw.MODEL.Card.ResourceCard;
+import it.polimi.ingsw.MODEL.Card.Side;
 import it.polimi.ingsw.MODEL.ENUM.AnglesEnum;
+import it.polimi.ingsw.MODEL.ENUM.CentralEnum;
 
 //@Davide
 // Class for the single cell present in the matrix of GameField
 public class GameFieldSingleCell {
+    /*
+    provo a mettere la carta trasparente tc e a considerarla sempre come carta_down,
+    inizio carta trasparente
+     */
+ /*   private final Side tc_front_side = new Side(AnglesEnum.EMPTY, AnglesEnum.EMPTY, AnglesEnum.EMPTY, AnglesEnum.EMPTY, CentralEnum.NONE, CentralEnum.NONE, CentralEnum.NONE);
+    private final Side tc_back_side = new Side(AnglesEnum.EMPTY, AnglesEnum.EMPTY, AnglesEnum.EMPTY, AnglesEnum.EMPTY, CentralEnum.NONE, CentralEnum.NONE, CentralEnum.NONE);
+    private final PlayCard tc = new ResourceCard(tc_front_side, tc_back_side,false, 0);
+    //fine carta trasparente
+*/
     private boolean filled;
     private PlayCard card;
     private AnglesEnum value;
@@ -20,13 +32,18 @@ public class GameFieldSingleCell {
     public boolean isEmpty() {
         return !filled;
     }
+
+    public boolean isFilled() { //ritorna 1 se c'è la carta metodo aggiunto -mirko-
+        return filled;
+    }
+
     public void setFilled(boolean filled) {
         this.filled = filled;
     }
     public PlayCard getCard() {
         if ( filled ) return card;
         else {
-        System.out.println("ERROR: THERE IS NO CARD HERE pos " + value);
+        System.out.println("ERROR: THERE IS NO CARD HERE pos " + value); //value = valore dell'angolo nella cella
         return null;}
     }
     public void setCard(PlayCard card) {
@@ -40,9 +57,17 @@ public class GameFieldSingleCell {
     }
 
     public PlayCard getCardDown() {
-        if ( !isEmpty() ) return card_down;
-        System.out.print("ERROR: THERE IS NO CARD HERE");
-        return null;
+        if(card_down.equals(carta_trasparente)){
+            //bisogna trovare il modo di mettere un controllo che checka se card_down=carta trasparente
+            //se vero allora comunque posso mettere giu una carta
+            return card_down;
+        }
+        else{
+            if ( !isEmpty() ) return card_down;
+            System.out.println("ERROR: THERE IS NO CARD HERE");
+            return null;
+        }
+
     }
     public void setCardDown(PlayCard card) {
         this.card_down = card;
