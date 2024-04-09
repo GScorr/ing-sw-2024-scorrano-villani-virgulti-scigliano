9031db650407f0e3a61c7649718c108cfa9289d2
GScorr <gabriele.scorrano@mail.polimi.it>
2024-04-09 15:55:04 +0200

    fix su deckcreation

--- CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/Game/DeckCreationTestGoal.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/Game/DeckCreationTestGoal.java
@@ -1,18 +1,24 @@
 package it.polimi.ingsw.MODEL.Game;
 
+import it.polimi.ingsw.MODEL.Card.PlayCard;
 import it.polimi.ingsw.MODEL.DeckPackage.Deck;
 import it.polimi.ingsw.MODEL.Goal.Goal;
 import org.junit.jupiter.api.Test;
 
+import java.util.Deque;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckCreationTestGoal {
     DeckCreation deck = new DeckCreation();
+    Deque<Goal> goals;
+    Deque<PlayCard> gold;
     @Test
     void creteGoalDeck() {
-
-        deck.getGoalDeck()
+        goals=deck.getGoalDeck();
+        for(Goal g:goals){
+            System.out.println(g.getGoalType() + "e" + g.getResource());
+        }
     }
 }
