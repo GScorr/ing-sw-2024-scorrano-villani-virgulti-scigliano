b87d367d991a49feca70243e7d29ad7b9eb8b528
Mirko <mirko.scigliano9@gmail.com>
2024-04-09 15:35:50 +0200

    test delle classi deck mancante deck goal

---
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/DeckPackage/DeckGoalCardTest.java
@@ -0,0 +1,23 @@
+package it.polimi.ingsw.MODEL.DeckPackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeckGoalCardTest {
+    /*
+    mi sa che non lo testo perche è identico, magari quando è pronto il json faccio le stesse prove di deck
+     */
+
+    @Test
+    void drawCard() {
+    }
+
+    @Test
+    void seeFirstCard() {
+    }
+
+    @Test
+    void getNumber() {
+    }
+}
