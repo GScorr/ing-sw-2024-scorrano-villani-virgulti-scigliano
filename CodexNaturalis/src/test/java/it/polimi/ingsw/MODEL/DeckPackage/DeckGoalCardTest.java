26b030c7ec1c5562fd7a50b75ccc2ff8290e92c7
Mirko <mirko.scigliano9@gmail.com>
2024-04-09 15:38:33 +0200

    test delle classi deck mancante deck goal

--- CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/DeckPackage/DeckGoalCardTest.java
+++ CodexNaturalis/src/test/java/it/polimi/ingsw/MODEL/DeckPackage/DeckGoalCardTest.java
@@ -1,12 +1,25 @@
 package it.polimi.ingsw.MODEL.DeckPackage;
 
+import it.polimi.ingsw.MODEL.Card.PlayCard;
+import it.polimi.ingsw.MODEL.Game.DeckCreation;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class DeckGoalCardTest {
+
+    DeckCreation creation = new DeckCreation();
+
+    Deck resource_deck = new Deck(creation.getMixResourcesDeck());
+    Deck gold_deck = new Deck(creation.getMixGoldDeck());
+
+    List<PlayCard> card_in_hands = new ArrayList<>();
+    List<PlayCard> carte_oro_in_mano = new ArrayList<>();
     /*
-    mi sa che non lo testo perche è identico, magari quando è pronto il json faccio le stesse prove di deck
+    in attesa che finisca la classe deckcreation con il deck goal
      */
 
     @Test
