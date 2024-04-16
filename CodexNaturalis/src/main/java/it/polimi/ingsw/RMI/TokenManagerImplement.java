064a5f7463761bf0dd98249d920185280352ba7b
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-04-16 14:28:25 +0200

    modifiche RMI, aggiunta gestione Token

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/RMI/TokenManagerImplement.java
@@ -0,0 +1,26 @@
+package it.polimi.ingsw.RMI;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class TokenManagerImplement implements TokenManager{
+
+    public Map<String, VirtualView> tokens = new HashMap<>();
+    @Override
+    public String generateToken(VirtualView clientId) {
+        String token = UUID.randomUUID().toString(); // Generate a casual token
+        tokens.put(token,clientId); // save client token association
+        return token;
+    }
+
+    @Override
+    public boolean validateToken(RmiClient clientId, String token) {
+        return tokens.containsKey(clientId) && tokens.get(clientId).equals(token);
+    }
+
+    public Map<String, VirtualView> getTokens(){
+        return this.tokens;
+    }
+
+}
