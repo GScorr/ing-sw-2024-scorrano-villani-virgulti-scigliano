dcc6626c810a607cb97e71637e2ef1068c08fb2a
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-04-20 20:21:34 +0200

    continuazione rmi final + discussione con gabri su implementazione funzionalità rmi

--- CodexNaturalis/src/main/java/it/polimi/ingsw/RMI_FINAL/VirtualServerF.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/RMI_FINAL/VirtualServerF.java
@@ -3,6 +3,7 @@ package it.polimi.ingsw.RMI_FINAL;
 
 import it.polimi.ingsw.CONTROLLER.GameController;
 import it.polimi.ingsw.MODEL.Player.Player;
+import it.polimi.ingsw.RMI.VirtualView;
 
 import java.rmi.Remote;
 import java.rmi.RemoteException;
@@ -10,12 +11,12 @@ import java.util.List;
 import java.util.Map;
 
 public interface VirtualServerF extends Remote {
-    public void connect(VirtualViewF client) throws RemoteException;
-
-
-    public boolean gamesIsEmpty() throws RemoteException;
-
+    public void connect(VirtualViewF client)throws RemoteException;
     public String createToken(VirtualViewF client ) throws  RemoteException;
-
-    public Player getFromToken(String token) throws  RemoteException;
+    public Map<String, Player> getTtoP() throws RemoteException;
+    public Map<String, GameController> getTtoG() throws RemoteException;
+    public List<GameController> getListGameController() throws  RemoteException;
+    public void CreatePlayer(String name, String client_token, boolean first) throws RemoteException;
+    public void createGame(int num_player, String p_token) throws RemoteException;
+    public void addPlayer() throws RemoteException;
 }
