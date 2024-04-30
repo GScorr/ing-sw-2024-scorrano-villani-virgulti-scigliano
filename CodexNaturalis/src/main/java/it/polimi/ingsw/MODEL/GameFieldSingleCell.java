954d80822744147b073a38a2f6c75bdc11cae687
GScorr <gabriele.scorrano@mail.polimi.it>
2024-04-30 11:20:10 +0200

    implementazione TUI gamefield (inizio)

--- CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/GameFieldSingleCell.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/GameFieldSingleCell.java
@@ -8,6 +8,7 @@ import it.polimi.ingsw.MODEL.ENUM.CentralEnum;
 import it.polimi.ingsw.MODEL.ENUM.EdgeEnum;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 //@Davide
@@ -26,8 +27,8 @@ public class GameFieldSingleCell implements Serializable {
     private PlayCard card;
     private AnglesEnum value;
     private PlayCard card_down;
-    private List<AnglesEnum> values; //overusage of variables, but useful in gamefield printing
-    private List<EdgeEnum> edges;
+    private List<AnglesEnum> values = new ArrayList<>(); //overusage of variables, but useful in gamefield printing
+    private List<EdgeEnum> edges = new ArrayList<>();
 
 //card_down means the angles that the card will cover -mirko-
     public GameFieldSingleCell(boolean filled, PlayCard card, AnglesEnum value, PlayCard card_down) {
@@ -106,6 +107,10 @@ public class GameFieldSingleCell implements Serializable {
                 return "EM";
         }
         return " ";
+    }
+
+    public List<EdgeEnum> getEdges() {
+        return edges;
     }
 
     public void setValues(AnglesEnum value, int index) {
