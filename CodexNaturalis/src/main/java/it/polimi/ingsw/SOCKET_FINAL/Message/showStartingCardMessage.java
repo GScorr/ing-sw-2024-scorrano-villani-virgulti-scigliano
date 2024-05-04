cf37715ca261952974f1f8c7ce8c92622d7158ee
mirko <mirko.scigliano9@gmail.com>
2024-05-04 14:50:46 +0200

    aggiornamento + push modifiche di martedì

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/SOCKET_FINAL/Message/showStartingCardMessage.java
@@ -0,0 +1,44 @@
+package it.polimi.ingsw.SOCKET_FINAL.Message;
+
+import it.polimi.ingsw.SOCKET.GiocoProva.Controller;
+import it.polimi.ingsw.SOCKET_FINAL.Server;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class showStartingCardMessage implements Message, Serializable {
+
+    /**
+     * ping temporizzato per vedere se il client è ancora collegato
+     * anche via rmi in modo che tutti siano a conoscenza di eventuali crush
+     */
+
+    public String token;
+
+
+    public Controller controller;
+    public Server server;
+
+    ObjectOutputStream output;
+    public void setController(Controller controller) {
+        this.controller = controller;
+    }
+
+    public void setServer(Server server) {
+        this.server = server;
+    }
+
+    public void setOutput(ObjectOutputStream output) {
+        this.output = output;
+    }
+
+    public showStartingCardMessage(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public void action() throws IOException {
+
+    }
+}
