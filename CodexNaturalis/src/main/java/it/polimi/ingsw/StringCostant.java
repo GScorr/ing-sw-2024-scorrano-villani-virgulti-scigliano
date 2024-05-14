9df640e389db9a483af57d81c6b21f3ad1806c68
francescoVirgulti <franci.virgulti@gmail.com>
2024-05-14 13:06:15 +0200

    Creation English String

---
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/StringCostant.java
@@ -0,0 +1,29 @@
+package it.polimi.ingsw;
+
+public  class StringCostant {
+
+    public String choose_name_player = "\nChoose name Player > ";
+    public String name_is_valid = "Name is valid\n";
+    public String name_is_not_valid = "Name already selected, please choose anotherone\n";
+    public String error = "\n Error, please insert a correct input value";
+    public String riconnected = "riconnected \n";
+    public String creation_player = "Player Created, Welcome to the Codex\n";
+    public String new_game_creation = "Not Game Available, create a new one \n Choose Game Name > ";
+    public String new_game_num_player = "Choose Game Number Player (From 2 to 4) > \n";
+    public String game_creation = "\nChoose Game Name > ";
+    public String new_or_old = "Digit 'new' to create a new Game, 'old' to enter in a existing game\n";
+
+    public String list_game_avilable = "\n List of available Games :";
+
+    public String Id_game = "\nInsert the ID of the game you would like to enter ";
+    public String enter ="Congratulation, you are enter in a Game !";
+    public String starting_card = "Choose Starting Card";
+    public String insert_starting_card = "\n Choose Side Starting Card:\n";
+    public String BackorFront = "\nInsert B for BackSide, F for FrontSide";
+
+    public String waitTurn = "\nWait for your turn ";
+    public String insertHandsCard = "\nInsert the card, this is your cards in hand: ";
+
+
+
+}
