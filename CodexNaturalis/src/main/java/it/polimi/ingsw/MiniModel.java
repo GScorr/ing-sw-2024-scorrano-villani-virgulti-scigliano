f7502f7e3c99b96f6a022d1260be8d141002eec1
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-05-14 22:29:42 +0200

    inizio mini model

--- CodexNaturalis/src/main/java/it/polimi/ingsw/MiniModel.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MiniModel.java
@@ -14,7 +14,7 @@ import java.util.concurrent.LinkedBlockingDeque;
 
 public class MiniModel {
     List<GameField> game_fields ;
-    private List<PlayCard> cards_in_hand ;
+    private List<PlayCard> cards_in_hand;
 
     private PlayerState player_state;
 
