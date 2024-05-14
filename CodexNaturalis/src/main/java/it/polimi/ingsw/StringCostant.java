7c8f4d5df1fd3e638cb340d48201ad295b771b59
francescoVirgulti <franci.virgulti@gmail.com>
2024-05-14 17:20:02 +0200

    Inserimento Gui

--- CodexNaturalis/src/main/java/it/polimi/ingsw/StringCostant.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/StringCostant.java
@@ -7,7 +7,7 @@ public  class StringCostant {
     public String name_is_not_valid = "Name already selected, please choose anotherone\n";
     public String error = "\n Error, please insert a correct input value";
     public String riconnected = "riconnected \n";
-    public String creation_player = "Player Created, Welcome to the Codex\n";
+    public String creation_player = "Player Created, Welcome to Codex\n";
     public String new_game_creation = "Not Game Available, create a new one \n Choose Game Name > ";
     public String new_game_num_player = "Choose Game Number Player (From 2 to 4) > \n";
     public String game_creation = "\nChoose Game Name > ";
@@ -16,7 +16,7 @@ public  class StringCostant {
     public String list_game_avilable = "\n List of available Games :";
 
     public String Id_game = "\nInsert the ID of the game you would like to enter ";
-    public String enter ="Congratulation, you are enter in a Game !";
+    public String enter ="Congratulation, you Game is starting ... ";
     public String starting_card = "Choose Starting Card";
     public String insert_starting_card = "\n Choose Side Starting Card:\n";
     public String BackorFront = "\nInsert B for BackSide, F for FrontSide";
@@ -24,6 +24,7 @@ public  class StringCostant {
     public String waitTurn = "\nWait for your turn ";
     public String insertHandsCard = "\nInsert the card, this is your cards in hand: ";
 
-
-
+    public String chooseIndex= "\nChoosed index (1,2,3): ";
+    public String insertCoordinate = "\nInsert coordinate x y  ";
+    public String drawFromAvailablaDeck = "\n Draw a card, deck available: ";
 }
