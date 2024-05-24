64f5a28c05068924335a91927663719453cc448f
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-05-24 16:56:50 +0200

    fix bugs disconnessione

--- CodexNaturalis/src/main/java/it/polimi/ingsw/CONSTANTS/Constants.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/CONSTANTS/Constants.java
@@ -3,5 +3,5 @@ package it.polimi.ingsw.CONSTANTS;
 public class Constants {
     public final static int MATRIXDIM = 45;
 
-    public final static String IPV4 = "127.0.0.1";
+    public final static String IPV4 = "192.168.39.78";
 }
