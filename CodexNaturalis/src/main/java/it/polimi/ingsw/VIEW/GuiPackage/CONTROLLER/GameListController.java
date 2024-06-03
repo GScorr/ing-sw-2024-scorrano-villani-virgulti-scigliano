7a6ef4b6a7766d5daf920216357bebc39e8994d3
francescoVirgulti <franci.virgulti@gmail.com>
2024-06-04 01:29:37 +0200

    La lista di free games è vista dalla GUI

--- CodexNaturalis/src/main/java/it/polimi/ingsw/VIEW/GuiPackage/CONTROLLER/GameListController.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/VIEW/GuiPackage/CONTROLLER/GameListController.java
@@ -1,6 +1,8 @@
 package it.polimi.ingsw.VIEW.GuiPackage.CONTROLLER;
 
 import it.polimi.ingsw.RMI_FINAL.SocketRmiControllerObject;
+import it.polimi.ingsw.RMI_FINAL.VirtualViewF;
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
@@ -10,21 +12,44 @@ import java.util.List;
 
 public class GameListController extends GenericSceneController{
 
+
+
     @FXML
     private VBox gameListContainer;
 
     private List<SocketRmiControllerObject> games;
 
     public void initialize() throws IOException, ClassNotFoundException, InterruptedException {
-        while(client == null){
-            Thread.sleep(500);
-            System.out.println("ciao");
-        }
-        System.out.println("esco");
-        // Call the client method to get the list of free games
-        games = client.getFreeGames();
 
-        // Create a button for each game and add it to the VBox
+
+        new Thread(() -> {
+            while (true) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                try {
+                    if (super.client != null) {
+                        games = client.getFreeGames();
+                        Platform.runLater(() -> updateGameList(games));
+                        break;
+                    };
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                } catch (ClassNotFoundException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }).start();
+
+    }
+
+    // Create a button for each game and add it to the VBox
+    private void updateGameList(List<SocketRmiControllerObject> games) {
+        gameListContainer.getChildren().clear(); // Clear existing buttons
         for (SocketRmiControllerObject game : games) {
             Button button = new Button(game.name + " - Players: " + game.num_player + "/" + game.max_num_player);
             button.setOnAction(event -> handleGameButtonClick(game));
@@ -37,4 +62,5 @@ public class GameListController extends GenericSceneController{
         System.out.println("Joining game: " + game.name);
         // You can add more logic here to join the selected game
     }
+
 }
