d35be40b5b0c065059efb9e2261c2cda3aad4bcb
Mirko <mirko.scigliano9@gmail.com>
2024-06-10 09:57:14 +0200

    Aggiornamento

--- CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/Card/Side.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/Card/Side.java
@@ -4,22 +4,40 @@ import it.polimi.ingsw.MODEL.ENUM.CentralEnum;
 
 import java.io.Serializable;
 
-/*
-*  @Francesco_Virgulti
-* central_resources_list è passato dal main solamente se la carta è una StartingCard
-* In tutte le altre carte central_resources_list sarà nullo
-* Nel front_side della StartingCard central_resources_list sarà nullo
-* */
+
+/**
+ * Represents a side of a card in the game, containing corner angles and central resources.
+ *
+ * Note that `central_resource_list` is only
+ * relevant for the back side of StartingCard instances. It will be null for other
+ * card types and for the front side of StartingCards.
+ */
 public class Side implements Serializable {
+    /**
+     * rappresent each angles of the card
+     */
     private final AnglesEnum angle_right_up;
     private final AnglesEnum angle_right_down;
     private final AnglesEnum angle_left_up;
     private final AnglesEnum angle_left_down;
+    /**
+     * (optional) rappresent the central resources
+     */
     private final CentralEnum central_resource;
     private final CentralEnum central_resource2;
     private final CentralEnum central_resource3;
 
-
+    /**
+     * Creates a new Side with corner angles and central resources.
+     *
+     * @param angle_right_up Angle of the top right corner.
+     * @param angle_right_down Angle of the bottom right corner.
+     * @param angle_left_up Angle of the top left corner.
+     * @param angle_left_down Angle of the bottom left corner.
+     * @param central_resource Optional central resource type (may be null).
+     * @param central_resource2 Optional second central resource type (may be null).
+     * @param central_resource3 Optional third central resource type (may be null).
+     */
     public Side(AnglesEnum angle_right_up, AnglesEnum angle_right_down, AnglesEnum angle_left_up, AnglesEnum angle_left_down, CentralEnum central_resource, CentralEnum central_resource2, CentralEnum central_resource3) {
         this.angle_right_up = angle_right_up;
         this.angle_right_down = angle_right_down;
