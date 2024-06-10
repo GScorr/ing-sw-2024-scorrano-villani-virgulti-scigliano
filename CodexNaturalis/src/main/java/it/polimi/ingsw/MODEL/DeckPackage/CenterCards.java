106d0d920c5aea90feecba2031f5c84b8724ffa1
Mirko <mirko.scigliano9@gmail.com>
2024-06-10 10:35:08 +0200

    Revisione commenti deck e enum

--- CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/DeckPackage/CenterCards.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/DeckPackage/CenterCards.java
@@ -5,12 +5,32 @@ import it.polimi.ingsw.MODEL.Card.PlayCard;
 import java.io.Serializable;
 import java.util.List;
 
+/**
+ * Manages the 2 decks and the 4 central cards from which it is possible to draw Gold and Resource cards.
+ */
 public class CenterCards implements Serializable {
+    /**
+     * 2 face-up gold card
+     */
     private List<PlayCard>  gold_list;
+    /**
+     * 2 face-up resource card
+     */
     private List<PlayCard> resource_list;
+    /**
+     * Deck for drawing new Cards.
+     */
     private Deck gold_deck;
     private Deck resources_deck;
 
+    /**
+     * Creates a new CenterCards instance.
+     *
+     * @param gold_list
+     * @param resource_list
+     * @param gold_deck Deck for drawing new Gold Cards.
+     * @param resources_deck Deck for drawing new Resource Cards.
+     */
     public CenterCards(List<PlayCard> gold_list, List<PlayCard> resource_list, Deck gold_deck, Deck resources_deck) {
         this.gold_list = gold_list;
         this.resource_list = resource_list;
@@ -41,6 +61,13 @@ public class CenterCards implements Serializable {
         return resource_list;
     }
 
+    /**
+     * Draws a Gold Card from the face-up, refilling it from the deck if empty.
+     *
+     * @param index Index of the card to draw.
+     * @return The drawn Gold Card.
+     * @throws EmptyDeckException If the Gold deck is empty.
+     */
     public PlayCard drawGoldCard(int index){
         if(gold_deck.cards.size() > 0){
             PlayCard tmp = gold_list.get(index);
@@ -55,6 +82,13 @@ public class CenterCards implements Serializable {
         }
     }
 
+    /**
+     * Draws a Resource Card from the face-up, refilling it from the deck if empty.
+     *
+     * @param index Index of the card to draw.
+     * @return The drawn Resource Card.
+     * @throws EmptyDeckException If the Resource deck is empty.
+     */
     public PlayCard drawResourceCard(int index){
         if(!resource_list.isEmpty()){
             PlayCard tmp = resource_list.get(index);
@@ -66,12 +100,20 @@ public class CenterCards implements Serializable {
 
     }
 
-    //If a GoldCard has been took from drawGoldCard a Card has to be drawed from the GoldDeck
+    /**
+     * If a GoldCard has been took from drawGoldCard a Card has to be drawed from the GoldDeck
+     * @param index
+     * @return
+     */
     private PlayCard insertFromGoldDeck(int index){
         return gold_deck.drawCard();
     }
 
-    //If a Resourced Card has been took from drawGoldCard a Card has to be drawed from the ResourceDeck
+    /**
+     * If a Resourced Card has been took from drawGoldCard a Card has to be drawed from the ResourceDeck
+     * @param index
+     * @return
+     */
     private PlayCard insertFromResourcesDeck(int index){
         return resources_deck.drawCard();
     }
