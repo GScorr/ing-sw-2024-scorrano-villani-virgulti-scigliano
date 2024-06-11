66ec7a0b3e47d7c6122b57869308c1acf7364f02
Mirko <mirko.scigliano9@gmail.com>
2024-06-11 16:07:13 +0200

    Revisione Commenti fino a Player esclusa

--- CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/Goal/GoalStrategy.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/Goal/GoalStrategy.java
@@ -3,7 +3,20 @@ package it.polimi.ingsw.MODEL.Goal;
 import it.polimi.ingsw.MODEL.ENUM.AnglesEnum;
 import it.polimi.ingsw.MODEL.GameField;
 
+/**
+ * Interface defining a strategy for calculating points based on game goals.
+ *
+ */
 public interface GoalStrategy {
+
+    /**
+     * Calculates the total points based on a specific goal strategy.
+     *
+     * @param field the player's game field
+     * @param points the base point value for the goal
+     * @param resource the type of resource or card color that might be relevant to the strategy
+     * @return the total number of points earned based on the implemented goal strategy
+     */
     int totalPoints(GameField field, int points, AnglesEnum resource);
 
 }
--- CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/Player/State/InvalidStateException.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/MODEL/Player/State/InvalidStateException.java
@@ -1,7 +1,18 @@
 package it.polimi.ingsw.MODEL.Player.State;
 
+/**
+ *  An exception class thrown when an invalid state operation is attempted.
+ *
+ */
 public class InvalidStateException extends RuntimeException {
+
+    /**
+     *  Constructor to create an `InvalidStateException` with a specific message.
+     *
+     *  @param message the message describing the reason for the exception
+     */
     public InvalidStateException(String message) {
         super(message);
     }
+
 }
