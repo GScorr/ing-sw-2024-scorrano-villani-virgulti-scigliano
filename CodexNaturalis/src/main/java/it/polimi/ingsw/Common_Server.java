12188d3ee71d33f7e30e7146a5d8dd4b59a00ca6
Villanidav <160397707+Villanidav@users.noreply.github.com>
2024-06-19 16:02:21 +0200

    fix definitivo disocnnessione???????

--- CodexNaturalis/src/main/java/it/polimi/ingsw/Common_Server.java
+++ CodexNaturalis/src/main/java/it/polimi/ingsw/Common_Server.java
@@ -213,11 +213,12 @@ public class Common_Server {
         long currentTime = System.currentTimeMillis();
         Set<String> keys = lastHeartbeatTime.keySet();
         for (String key : keys) {
-            if (currentTime - lastHeartbeatTime.get(key) > HEARTBEAT_TIMEOUT) {
+            if (currentTime - lastHeartbeatTime.get(key) > HEARTBEAT_TIMEOUT && ( lastHeartbeatTime.get(key) != 0 ) ) {
                 try{
                     if(token_to_rmi.get(key)!=null ){if( token_to_rmi.get(key).getTtoP().get(key).isDisconnected()) continue;
                     token_to_rmi.get(key).getTtoP().get(key).disconnect();
                     System.out.println(token_to_rmi.get(key).getTtoP().get(key).getName() + " disconnected");
+                        lastHeartbeatTime.put(key, 0L);
                     token_manager.deleteVW(key);}
                         if(token_to_rmi.get(key)!=null ){try{token_to_rmi.get(key).checkEndDisconnect();}catch (ConcurrentModificationException ignored){}
                     token_to_rmi.get(key).clientsRMI.remove( token_to_rmi.get(key).token_manager.getTokens().get(key)  );
